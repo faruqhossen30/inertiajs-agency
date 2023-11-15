3ef356b44305764696bc7d92cf72829120682a4e
faruqhossen30 <faruqhossen30@gmail.com>
2023-11-15 22:28:36 +0600

    svg problem fix

--- resources/js/Pages/HomePage.jsx
+++ resources/js/Pages/HomePage.jsx
@@ -1,12 +1,9 @@
 import AppLayout from '@/Layouts/AppLayout';
 import { Link, Head } from '@inertiajs/react';
-import Dropdown from '@/Components/Dropdown';
-import Slider from '@/Components/Slider';
 import HeroSection from '@/Components/Homepage/HeroSection';
 import ServiceSection from '@/Components/Homepage/ServiceSection';
 import FeatureStatus from '@/Components/Homepage/FeatureStatus';
 import Feature from '@/Components/Homepage/Feature';
-import Client from '@/Components/Homepage/Client';
 import Testimonial from '@/Components/Homepage/Testimonial';
 
 export default function Homepage({ auth, user, laravelVersion, phpVersion }) {
@@ -14,12 +11,11 @@ export default function Homepage({ auth, user, laravelVersion, phpVersion }) {
     return (
         <AppLayout>
             <Head title="Homepage" />
-            <HeroSection/>
-            <ServiceSection/>
-            <FeatureStatus/>
-            <Feature/>
-            <Client/>
-            <Testimonial/>
+            <HeroSection />
+            <ServiceSection />
+            <FeatureStatus />
+            <Feature />
+            <Testimonial />
         </AppLayout>
     );
 }
