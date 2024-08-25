3408ad8ad21a25c5627b5e6809c0dd25ef6b4d46
faruqhossen30 <faruqhossen30@gmail.com>
2024-08-25 19:50:56 +0600

    footer section added

--- resources/js/Layouts/Footer.jsx
+++ resources/js/Layouts/Footer.jsx
@@ -48,71 +48,84 @@ export default function Footer({ auth }) {
                 </div>
             </footer>
 
-            <section className="w-full border-t dark:border-t-gray-700">
+            <section className="w-full border-t dark:border-t-gray-700 bg-gray-950">
                 <footer className="hidden container lg:block py-5 px-4 sm:px-6 lg:px-8 mx-auto">
 
-                    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 mb-10">
-                        <div className="col-span-full hidden lg:col-span-1 lg:block">
-                            <a className="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">Boost Care IT</a>
-                            <p className="mt-3 text-xs sm:text-sm text-gray-600 dark:text-gray-400">© {new Date().getFullYear()} All rights reserved.</p>
-                        </div>
+                    <div className="grid grid-cols-12 py-16">
+                        <div className="col-span-3">
+                            <div className=" space-y-6">
+                                <div className=" flex items-center space-x-3 text-sm">
+                                    <img className='h-6' src={window.location.origin + '/fc0cd06a-0b05-4ced-af75-7fcc74579754.jpeg'} alt="" srcset="" />
+                                    <h1 className=' text-white capitalize font-bold text-xl'>pstls.</h1>
+                                </div>
 
+                                <p className='text-sm text-white leading-7'>Lorem ipsum dolor, sit amet<br /> consectetur adipisicing   quis.</p>
+                                <div className="flex space-x-2">
+                                    <svg className="w-6 h-6 text-gray-50 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z" />
+                                    </svg>
+                                    <svg className="w-6 h-6 text-gray-50 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd" />
+                                        <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
+                                    </svg>
 
-                        <div>
-                            <h4 className="text-xs font-semibold text-gray-900 uppercase dark:text-gray-100">Product</h4>
-
-                            <div className="mt-3 grid space-y-3 text-sm">
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Pricing</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Changelog</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Docs</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Download</a></p>
-                            </div>
-                        </div>
-                        <div>
-                            <h4 className="text-xs font-semibold text-gray-900 uppercase dark:text-gray-100">Company</h4>
-
-                            <div className="mt-3 grid space-y-3 text-sm">
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">About us</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Blog</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Careers</a> <span className="inline text-blue-600 dark:text-blue-500">— We're hiring</span></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Customers</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Newsroom</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Sitemap</a></p>
-                            </div>
-                        </div>
-
-
-                        <div>
-                            <h4 className="text-xs font-semibold text-gray-900 uppercase dark:text-gray-100">Resources</h4>
-
-                            <div className="mt-3 grid space-y-3 text-sm">
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Community</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Help & Support</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">eBook</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">What's New</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Status</a></p>
-                            </div>
-                        </div>
-
-
-                        <div>
-                            <h4 className="text-xs font-semibold text-gray-900 uppercase dark:text-gray-100">Developers</h4>
-
-                            <div className="mt-3 grid space-y-3 text-sm">
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Api</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Status</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">GitHub</a> <span className="inline text-blue-600 dark:text-blue-500">— New</span></p>
+                                    <svg className="w-6 h-6 text-gray-50 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path fill="currentColor" fill-rule="evenodd" d="M12 4a8 8 0 0 0-6.895 12.06l.569.718-.697 2.359 2.32-.648.379.243A8 8 0 1 0 12 4ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.96 9.96 0 0 1-5.016-1.347l-4.948 1.382 1.426-4.829-.006-.007-.033-.055A9.958 9.958 0 0 1 2 12Z" clip-rule="evenodd" />
+                                        <path fill="currentColor" d="M16.735 13.492c-.038-.018-1.497-.736-1.756-.83a1.008 1.008 0 0 0-.34-.075c-.196 0-.362.098-.49.291-.146.217-.587.732-.723.886-.018.02-.042.045-.057.045-.013 0-.239-.093-.307-.123-1.564-.68-2.751-2.313-2.914-2.589-.023-.04-.024-.057-.024-.057.005-.021.058-.074.085-.101.08-.079.166-.182.249-.283l.117-.14c.121-.14.175-.25.237-.375l.033-.066a.68.68 0 0 0-.02-.64c-.034-.069-.65-1.555-.715-1.711-.158-.377-.366-.552-.655-.552-.027 0 0 0-.112.005-.137.005-.883.104-1.213.311-.35.22-.94.924-.94 2.16 0 1.112.705 2.162 1.008 2.561l.041.06c1.161 1.695 2.608 2.951 4.074 3.537 1.412.564 2.081.63 2.461.63.16 0 .288-.013.4-.024l.072-.007c.488-.043 1.56-.599 1.804-1.276.192-.534.243-1.117.115-1.329-.088-.144-.239-.216-.43-.308Z" />
+                                    </svg>
+                                    <svg className="w-6 h-6 text-gray-50 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path fill="currentColor" fill-rule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clip-rule="evenodd" />
+                                    </svg>
+                                    <svg className="w-6 h-6 text-gray-50 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                        <path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
                             </div>
 
-                            <h4 className="mt-7 text-xs font-semibold text-gray-900 uppercase dark:text-gray-100">Industries</h4>
-
-                            <div className="mt-3 grid space-y-3 text-sm">
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Financial Services</a></p>
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200" href="#">Education</a></p>
-                            </div>
                         </div>
+                        <div className="col-span-3">
+                            <h6 className='text-white  font-bold pb-5'>Product</h6>
+                            <p><a href="#" className='text-white text-sm'>Hello</a></p>
+                            <p><a href="#" className='text-white text-sm'>Story</a></p>
+                            <p><a href="#" className='text-white text-sm'>Pricing</a></p>
+                            <p><a href="#" className='text-white text-sm'>Applications</a></p>
+                        </div>
+                        <div className="col-span-3">
+                            <h6 className='text-white  font-bold pb-5'>Resources</h6>
+                            <p><a href="#" className='text-white  text-sm'>New Account</a></p>
+                            <p><a href="#" className='text-white  text-sm'>Log in</a></p>
+                            <p><a href="#" className='text-white  text-sm'>Testrmonials</a></p>
+                            <p><a href="#" className='text-white  text-sm'>Contact</a></p>
+
+                        </div>
+                        <div className="col-span-3">
+                            <div className="space-y-3 ">
+                            <h6 className='text-white  font-bold '>Subscribe</h6>
+                                <form>
+                                    <div class="mt-4 flex flex-col items-center gap-2 sm:flex-row sm:gap-3 bg-white p-2 dark:bg-neutral-900">
+                                        <div class="w-full">
+                                            <label for="hero-input" class="sr-only">Subscribe</label>
+                                            <input type="text" id="hero-input" name="hero-input" class="py-1 px-4 block w-full border-transparent text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none  " placeholder="Enter Address"/>
+                                        </div>
+                                        <a class="w-full sm:w-auto whitespace-nowrap p-2 inline-flex justify-center items-center gap-x-2 text-sm font-medium border border-transparent bg-gray-800 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="#">
+                                            Send
+                                        </a>
+                                    </div>
+
+                                </form>
+                            </div>
+
+                        </div>
+
                     </div>
+
                 </footer>
+                <div className=" my-3 bg-gray-900">
+                    <div className="py-12 flex justify-center">
+                        <p className='text-center text-gray-400'> @ Pstls. 2024 All right reserved.</p>
+                    </div>
+
+                </div>
             </section>
 
         </>
